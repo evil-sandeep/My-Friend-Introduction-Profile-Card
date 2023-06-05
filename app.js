bc18cd2e85779f6f59f94b3bb50a05543718a3cd
SANDEEP KUMAR SAHOO <107100994+evil-sandeep@users.noreply.github.com>
2023-06-05 22:07:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const heading = 'Meet my fascinating friend.';
+let i = 0;
+
+const typing = () => {
+    if (i < heading.length) {
+        document.querySelector('.heading').innerHTML += heading.charAt(i);
+        i++;
+
+        setTimeout(typing, 150);
+    }
+}
+
+typing();
